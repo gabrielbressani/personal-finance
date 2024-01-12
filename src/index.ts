33a1c16a857c260de211cb506427b89770d1f0c5
gabriel.ribeiro <gabriel.ribeiro@willbank.com.br>
2024-01-12 19:12:14 -0300

    Atualiza menu com novas opções e adiciona função para preparar planilha.

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,22 @@ import { CategoryView } from './application/category/CategoryView';
 
 function onOpen() {
   const menu = SpreadsheetApp.getUi().createAddonMenu();
-  // menu.addItem('Cadastrar novas contas', 'addNewAccounts');
+  menu.addItem('Peparar planilha para o primeiro uso', 'prepareSpreadsheet');
+  menu.addSeparator();
+  menu.addItem('Cadastrar novas contas', 'addNewAccounts');
   menu.addItem('Pedir atualização manual de contas', 'requestAccountsRefresh');
   menu.addSeparator();
+  menu.addItem('Checar reembolsos', 'checkRefunds');
+  menu.addSeparator();
   menu.addItem('Atualizar dashboard', 'updateAccountsBalance');
+  menu.addSeparator();
+  menu.addItem('Atualizar todas as abas', 'refreshAllTabs');
   menu.addSeparator();
   menu.addItem('Atualizar transações de crédito', 'appendNewCreditTransactions');
   menu.addItem('Atualizar transações de conta corrente', 'appendNewCheckingAccountTransactions');
   menu.addSeparator();
   menu.addItem('Atualizar categorias', 'updateCategories');
   menu.addToUi();
-}
-
-function addNewAccounts() {
-  SpreadsheetApp.getUi().alert('Não implementado');
 }
 function requestAccountsRefresh() {
   new AccountView().requestAccountsRefresh();
@@ -39,4 +41,19 @@ function updateAccountsBalance() {
 
 function updateCategories() {
   new CategoryView().list();
+}
+
+function prepareSpreadsheet() {
+  SpreadsheetApp.getUi().alert('Não implementado');
+}
+
+function checkRefunds() {
+  SpreadsheetApp.getUi().alert('Não implementado');
+}
+
+function addNewAccounts() {
+  SpreadsheetApp.getUi().alert('Não implementado');
+}
+function refreshAllTabs() {
+  SpreadsheetApp.getUi().alert('Não implementado');
 }
