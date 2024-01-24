7709a19682417f2b2a6e27b2b6af51d63aee6e1a
gabriel.ribeiro <gabriel.ribeiro@willbank.com.br>
2024-01-24 15:19:53 -0300

    Simplify menu structure in index.ts file
    Removed several functions and menu items from the main application menu to simplify navigation. The affected functions at this stage return a 'Not implemented' alert, and they were removed from the 'onOpen' function and the 'accountSubmenu' and 'settingsSubMenu' menus.

--- src/index.ts
+++ src/index.ts
@@ -5,28 +5,19 @@ import { CheckingAccountTransactionView } from './application/transaction/Checki
 import { CategoryView } from './application/category/CategoryView';
 
 function onOpen() {
-  const menu = SpreadsheetApp.getUi().createAddonMenu();
-
   const accountSubmenu = SpreadsheetApp.getUi().createMenu('Contas');
   accountSubmenu.addItem('Baixar dados das contas', 'requestAccountsRefresh');
-  accountSubmenu.addSeparator();
-  accountSubmenu.addItem('Atualizar todas as abas', 'refreshAllTabs');
   accountSubmenu.addSeparator();
   accountSubmenu.addItem('Atualizar cartão de crédito', 'appendNewCreditTransactions');
   accountSubmenu.addItem('Atualizar conta corrente', 'appendNewCheckingAccountTransactions');
   accountSubmenu.addItem('Atualizar dashboard', 'updateAccountsBalance');
-  menu.addSubMenu(accountSubmenu);
-
-  menu.addSeparator();
-  menu.addItem('Checar reembolsos', 'checkRefunds');
-  menu.addSeparator();
 
   const settingsSubMenu = SpreadsheetApp.getUi().createMenu('Configurações');
-  settingsSubMenu.addItem('Peparar planilha para o primeiro uso', 'prepareSpreadsheet');
-  settingsSubMenu.addItem('Cadastrar novas contas', 'addNewAccounts');
   settingsSubMenu.addItem('Atualizar categorias', 'updateCategories');
-  menu.addSubMenu(settingsSubMenu);
 
+  const menu = SpreadsheetApp.getUi().createAddonMenu();
+  menu.addSubMenu(accountSubmenu);
+  menu.addSubMenu(settingsSubMenu);
   menu.addToUi();
 }
 function requestAccountsRefresh() {
@@ -47,19 +38,4 @@ function updateAccountsBalance() {
 
 function updateCategories() {
   new CategoryView().list();
-}
-
-function prepareSpreadsheet() {
-  SpreadsheetApp.getUi().alert('Não implementado');
-}
-
-function checkRefunds() {
-  SpreadsheetApp.getUi().alert('Não implementado');
-}
-
-function addNewAccounts() {
-  SpreadsheetApp.getUi().alert('Não implementado');
-}
-function refreshAllTabs() {
-  SpreadsheetApp.getUi().alert('Não implementado');
 }
